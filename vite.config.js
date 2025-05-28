0da0fcc78ac4b6fd140575d39aa488c877e1aba4
XgromX33 <kubuse440@gmail.com>
2025-05-28 22:53:59 +0200

    Updated package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
+    react(),
     legacy({
       targets: ['defaults', 'not IE 11'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
